dcdfd1b68dfacb54690d832071e09b619111f014
Morya Breland Jr <morya.58@gmail.com>
2014-11-12 12:42:57 -0800

    11/12/2014 INFO 343 Class Work

--- js/app.js
+++ js/app.js
@@ -12,7 +12,7 @@
 //sending a DELETE to this URL + '/' + task.objectId will delete an existing task
 var tasksUrl = 'https://api.parse.com/1/classes/tasks';
 
-angular.module('ToDoApp', [])
+angular.module('ToDoApp', ['ui.bootstrap'])
     .config(function($httpProvider) {
         //Parse required two extra headers sent with every HTTP request: X-Parse-Application-Id, X-Parse-REST-API-Key
         //the first needs to be set to your application's ID value
@@ -59,6 +59,26 @@ angular.module('ToDoApp', [])
                 .error(function (err) {
                     $scope.errorMessage = err;
                 });
-        };
+        }; //end of updateTask()
+
+        $scope.incrementVotes = function (task, amount) {
+            $scope.updating = true;
+            $http.put(tasksUrl + '/' + task.objectId,
+                {votes: {
+                    __op: 'Increment',
+                    amount: amount
+                }
+            })
+                .success(function (responseData) {
+                    task.votes = responseData.votes;
+                })
+                .error(function (err) {
+                    $scope.errorMessage = err;
+                })
+                .finally(function () {
+                    $scope.loading = false;
+                })
+        }; //end of incrementVotes
     });
+
 
